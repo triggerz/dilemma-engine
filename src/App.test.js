00709c58b088bfe65569be67e704e16a58ed6302
Kristian Dupont <kristian@kristiandupont.com>
2017-12-05 09:41:23 +0100

    Add Enzyme for shallow rendering and add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const config = {
+    initialScene: 'initialScene',
+    scenes: {
+      'initialScene': {}
+    }
+  };
+  const variables = {};
+  shallow(<App config={config} variables={variables}/>);
 });
